15e067639f7512548b8194ae06cbca8e66e27c81
mikehochuli <mikehochuli@gmail.com>
2025-08-30 17:10:05 +0200

    light mode fixed on

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // initialization
 
+document.documentElement.classList.remove('tw-dark');
+localStorage.setItem("color-mode", "light");
+
 const RESPONSIVE_WIDTH = 1024
 
 let headerWhiteBg = false
@@ -67,39 +70,6 @@ function responsive() {
 responsive()
 window.addEventListener("resize", responsive)
 
-/** Dark and light theme */
-if (localStorage.getItem('color-mode') === 'dark' || (!('color-mode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('tw-dark')
-    updateToggleModeBtn()
-} else {
-    document.documentElement.classList.remove('tw-dark')
-    updateToggleModeBtn()
-}
-
-function toggleMode(){
-    //toggle between dark and light mode
-    document.documentElement.classList.toggle("tw-dark")
-    updateToggleModeBtn()
-    
-}
-
-function updateToggleModeBtn(){
-
-    const toggleIcon = document.querySelector("#toggle-mode-icon")
-    
-    if (document.documentElement.classList.contains("tw-dark")){
-        // dark mode
-        toggleIcon.classList.remove("bi-sun")
-        toggleIcon.classList.add("bi-moon")
-        localStorage.setItem("color-mode", "dark")
-        
-    }else{
-        toggleIcon.classList.add("bi-sun")
-        toggleIcon.classList.remove("bi-moon")
-        localStorage.setItem("color-mode", "light")
-    }
-
-}
 
 
 const promptWindow =  new Prompt("#pixa-playground")
