a6d708003be2b87a3fdcf2dbaeb1e2aef79644ce
mikehochuli <mikehochuli@gmail.com>
2025-08-31 20:17:25 +0200

    new tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  prefix: 'tw-',
+  important: false,
   content: [
     "./index.html",
-    "./**/*.{html,js}",
+    "./**/*.{html,js,jsx}"
   ],
-  prefix: 'tw-',
+  darkMode: false, // Nur Light Mode
   theme: {
     extend: {
       fontFamily: {
@@ -12,6 +14,7 @@ module.exports = {
         mont: ['Montserrat', 'sans-serif'],
       },
       colors: {
+        // Dynamische Farbwerte aus CSS-Variablen (Light Mode)
         primary: 'var(--color-primary)',
         'primary-hover': 'var(--color-primary-hover)',
         'primary-active': 'var(--color-primary-active)',
@@ -23,7 +26,6 @@ module.exports = {
 
         'text-primary': 'var(--color-text-primary)',
         'text-secondary': 'var(--color-text-secondary)',
-
         bg: 'var(--color-bg)',
         'bg-section': 'var(--color-bg-section)',
         'bg-hover': 'var(--color-bg-hover)',
@@ -37,5 +39,9 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('firefox', ':-moz-any(&)')
+    }
+  ],
 }
